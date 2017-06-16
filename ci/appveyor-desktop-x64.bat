24627e6f28fb596be417841b5affd1c795ba3e68
sigman <sigman78@gmail.com>
2017-06-16 22:02:01 +0300

    First appveyor build

---
+++ ci/appveyor-desktop-x64.bat
@@ -0,0 +1,9 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 || exit /b
+
+mkdir build && cd build || exit /b
+cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 15 2017 Win64"
+
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+
+call cmake\run_tests.bat || exit /b
