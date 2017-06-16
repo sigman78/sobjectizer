a663003b3cd72612710fd390ffe84659f3f34f96
sigman <sigman78@gmail.com>
2017-06-16 22:11:39 +0300

    Try Ninja as default generator

--- ci/appveyor-desktop-x64.bat
+++ ci/appveyor-desktop-x64.bat
@@ -1,9 +1,10 @@
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 || exit /b
 
 mkdir build && cd build || exit /b
-cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 15 2017 Win64"
+cmake .. -DBUILD_TESTS=ON -G Ninja
 
 cmake --build . || exit /b
 cmake --build . --target install || exit /b
 
-call cmake\run_tests.bat || exit /b
+cd ..
+:: call cmake\run_tests.bat || exit /b
